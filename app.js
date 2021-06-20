7412d036cd7c4382bc1151e5f1167b1d88ec3643
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-06-20 08:40:11 +0300

    - node js - nodemon - express

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const http = require('http')
 const express = require('express')
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the middleware!")
+app.use('/', (req, res, next) => {
+    console.log("Always middleware!")
     next()
 })
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
     console.log("In ANOTHER the middleware!")
+    res.send('<h1>Hello from Add Product Page</h1>')
+})
+app.use('/', (req, res, next) => {
+    console.log("In default the middleware!")
     res.send('<h1>Hello from Express</h1>')
 })
 
